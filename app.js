cc8556f54729a56bc291d12d982eefc082b135af
Jay <jordanchu95@gmail.com>
2020-05-23 17:32:40 -0400

    forgot to add math floor to function for random

--- app.js
+++ app.js
@@ -69,19 +69,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //move down function
     function moveDown() {
-        undraw()
-        currentPosition += width
-        draw()
-    }
+        undraw();
+        currentPosition += width;
+        draw();
+    };
 
     //freeze function
     function freeze() {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-            current.forEach(index => squares[currentPosition + index].classList.add('taken')])
-            random = Math.random() * theTetrominoes.length
-            current = theTetrominoes[random][currentRotation]
-            currentPosition = 4
-            draw() 
-        }
-    }
+            current.forEach(index => squares[currentPosition + index].classList.add('taken')]);
+            random = Math.floor(Math.random() * theTetrominoes.length);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw() ;
+        };
+    };
 });
